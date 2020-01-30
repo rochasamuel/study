20f849eaece01a2d854d5810bf9c7d3c66a2f079
Samuel da Silva Rocha <T1090440@interno.bb.com.br>
2020-01-30 17:31:24 -0300

    Terminando a parte 6

--- Alura - Java Parte 6/eclipse-workspace/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ Alura - Java Parte 6/eclipse-workspace/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -5,7 +5,7 @@ package br.com.bytebank.banco.modelo;
  * @author André Chaves
  * 
  */
-public abstract class Conta {
+public abstract class Conta implements Comparable<Conta> {
 
     protected double saldo;
     private int agencia;
@@ -109,5 +109,10 @@ public abstract class Conta {
     	}
     	
     	return true;
+    }
+
+    @Override
+    public int compareTo(Conta outra) {
+        return Double.compare(this.saldo, outra.saldo);
     }
 }
